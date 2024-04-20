5dc22507c5830f0d2c4f03eda20b09b5a0975a73
mkaufman94@gmail.com <mkaufman94@gmail.com>
2024-04-20 15:03:03 -0500

    feat: Load types as well and make script src a module

--- poke-api/src/script.js
+++ poke-api/src/script.js
@@ -1,13 +1,21 @@
-// import * as d3 from 'https://esm.sh/d3';
+import * as d3 from 'https://esm.sh/d3';
 
 const pokeAPIBaseUrl = 'https://pokeapi.co/api/v2';
 const getGen1PokemonSuffix = '/pokemon?limit=151';
+const getGen1PokemonGen1Suffix = '/generation/1';
 const getGen1PokemonUrl = `${pokeAPIBaseUrl}${getGen1PokemonSuffix}`;
+const getGen1PokemonGen1Url = `${pokeAPIBaseUrl}${getGen1PokemonGen1Suffix}`;
 
 document.addEventListener('DOMContentLoaded', async () => {
+  document.getElementById('pokemonNameArray').innerHTML = 'loading...';
+
+  const typesList = await fetchGen1PokemonTypes();
   const pokemonList = await fetchGen1Pokemon();
 
-  document.getElementById('pokemonNameArray').innerHTML = pokemonList;
+  console.log(typesList);
+  console.log(pokemonList);
+
+  document.getElementById('pokemonNameArray').innerHTML = 'loaded!';
 });
 
 async function fetchGen1Pokemon() {
@@ -20,10 +28,33 @@ async function fetchGen1Pokemon() {
   for (let i = 0; i < pokemonResults.length; i++) {
     const currentPokemon = pokemonResults[i];
 
-    console.log(currentPokemon);
+    const fullPokemonResponse = await fetch(currentPokemon.url);
+    const fullPokemonData = await fullPokemonResponse.json();
+    const { name, types } = fullPokemonData;
 
-    pokemonList.push(currentPokemon.name);
+    const currentPokemonTypes = [];
+    types.forEach((pokemonType) => {
+      currentPokemonTypes.push(pokemonType?.type?.name);
+    });
+
+    const relevantPokemonData = { name, types: currentPokemonTypes };
+
+    pokemonList.push(relevantPokemonData);
   }
 
   return pokemonList;
+}
+
+async function fetchGen1PokemonTypes() {
+  let typesList = [];
+
+  const response = await fetch(getGen1PokemonGen1Url);
+  const gen1PokemonTypesResponse = await response.json();
+
+  const types = gen1PokemonTypesResponse.types;
+  types.forEach((pokemonType) => {
+    typesList.push(pokemonType.name);
+  });
+
+  return typesList;
 }
