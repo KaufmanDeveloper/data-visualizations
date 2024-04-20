0490cd4952d12429c3dc5ac01333fe6e891ac21d
mkaufman94@gmail.com <mkaufman94@gmail.com>
2024-04-20 14:06:49 -0500

    fix: Refactor fetch pokemon names method

--- poke-api/src/script.js
+++ poke-api/src/script.js
@@ -1,18 +1,29 @@
 // import * as d3 from 'https://esm.sh/d3';
 
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+const pokeAPIBaseUrl = 'https://pokeapi.co/api/v2';
+const getGen1PokemonSuffix = '/pokemon?limit=151';
+const getGen1PokemonUrl = `${pokeAPIBaseUrl}${getGen1PokemonSuffix}`;
 
-let pokemonList = [];
+document.addEventListener('DOMContentLoaded', async () => {
+  const pokemonList = await fetchGen1Pokemon();
 
-document.addEventListener('DOMContentLoaded', () => {
-  fetch(url).then(async function (response) {
-    const responseJson = await response.json();
-    const pokemonResults = responseJson.results;
-    for (let i = 0; i < pokemonResults.length; i++) {
-      const currentPokemon = pokemonResults[i];
-      pokemonList.push(currentPokemon.name);
-    }
-
-    document.getElementById('pokemonNameArray').innerHTML = pokemonList;
-  });
+  document.getElementById('pokemonNameArray').innerHTML = pokemonList;
 });
+
+async function fetchGen1Pokemon() {
+  let pokemonList = [];
+
+  const response = await fetch(getGen1PokemonUrl);
+
+  const responseJson = await response.json();
+  const pokemonResults = responseJson.results;
+  for (let i = 0; i < pokemonResults.length; i++) {
+    const currentPokemon = pokemonResults[i];
+
+    console.log(currentPokemon);
+
+    pokemonList.push(currentPokemon.name);
+  }
+
+  return pokemonList;
+}
