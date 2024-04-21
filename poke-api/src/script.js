26ef1bb60063ec76fb9ff7a4cc2834c26b83e2e0
mkaufman94@gmail.com <mkaufman94@gmail.com>
2024-04-21 07:58:52 -0500

    feat: Include observable library

--- poke-api/src/script.js
+++ poke-api/src/script.js
@@ -1,14 +1,26 @@
-import * as d3 from 'https://esm.sh/d3';
+// import * as d3 from 'https://esm.sh/d3';
+import * as Plot from 'https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm';
 import { fetchGen1Pokemon, fetchGen1PokemonTypes } from './fetchPokemon.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  document.getElementById('pokemonNameArray').innerHTML = 'loading...';
+  document.getElementById('loading').innerHTML = 'loading...';
 
   const typesList = await fetchGen1PokemonTypes();
   const pokemonList = await fetchGen1Pokemon();
 
+  plotExample();
+
   console.log(typesList);
   console.log(pokemonList);
 
-  document.getElementById('pokemonNameArray').innerHTML = 'loaded!';
+  document.getElementById('loading').innerHTML = 'loaded!';
 });
+
+function plotExample() {
+  const plot = Plot.rectY(
+    { length: 10000 },
+    Plot.binX({ y: 'count' }, { x: Math.random })
+  ).plot();
+  const div = document.querySelector('#content');
+  div.append(plot);
+}
